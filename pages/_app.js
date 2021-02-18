a4cc2156f43cbfdea4476248898011c8535a38a0
Lucas Azevedo <lucasbazev@gmail.com>
2021-02-18 20:23:06 -0300

    initial version

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../css/styles.css';
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <Component {...pageProps} />
+  )
+}
+
+export default App;
---
+++ pages/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import getUser from '../utils/getUser';
+
+const Index = ({ repos, user }) => {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-5xl">Olá, eu sou o Lucas Azevedo!</h1>
+      <h2 className="font-bold text-3xl">Meus repositórios no GitHub:</h2>
+      <p>GitHub stats: Public repos: {user.public_repos} / Followers: {user.followers}</p>
+      {repos.map(repo => {
+        return (
+          <div key={repo.id} className="rounded bg-gray-200 mx-8 my-4 p-4 hover:shadow-md">
+            <h3 className="font-bold">{repo.name}</h3>
+            <p>Language: {repo.language} / Stars: {repo.stargazers_count}</p>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export async function getServerSideProps(context) {
+  const { repos, user } = await getUser('lucasbazev');
+
+  return {
+    props: {
+      repos,
+      user
+    }
+  }
+}
+
+export default Index;

060a8ac6d2b51fda8fd80e9454a9562c8a3a78da
Lucas Azevedo <lucasbazev@gmail.com>
2021-02-20 16:04:54 -0300

    purgecss problem

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      colors: {
-        newColor: '#F05E7B'
-      }
-    },
+    extend: {},
     fontFamily: {
       'sans': 'Inter'
     }

72094a00896adb32475b6d04b9f056e95d1e04ab
Lucas Azevedo <lucasbazev@gmail.com>
2021-02-24 14:27:55 -0300

    fixed minor issues

--- pages/repositorios.js
+++ pages/repositorios.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import getUser from '../utils/getUser';
 
 const Index = ({ repos, user }) => {
   return (
@@ -19,7 +18,8 @@ const Index = ({ repos, user }) => {
 }
 
 export async function getServerSideProps(context) {
-  const { repos, user } = await getUser('lucasbazev');
+  const request = fetch(process.env.API_URL + '/api/getUserData');
+  const { repos, user } = await request.json();   
 
   return {
     props: {
